b4df9fbb4e4cf7f6e849dde8038284f85a537d09
JackMcCrack <jack@localhost.localdomain>
2019-12-06 23:13:44 +0900

    step-by-step debug version

--- day5/day5-1.py
+++ day5/day5-1.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-n=0
-v=0
-out=0
 
 mass=open("input", "r")
 i=0
@@ -9,24 +6,12 @@ run=1
 for line in mass:
     a=line.split(",")
 a = [ int(x) for x in a ]
-a[1]=n
-a[2]=v
 while i<len(a) and run == 1:
     op=a[i]%100
-    A = (1 == int(a[i]/10000)%100)
+    A = (1 == int(a[i]/10000)%100) #immedate mode
     B = (1 == int(a[i]/1000)%10)
     C = (1 == int(a[i]/100)%2)
-    #print(a[i:i+4])
-    step=(a[i:i+4])
-    step[0]=op
-    
-    #pos mode
-    if A==False:
-        step[3]=a[step[3]]
-    if B==False:
-        step[2]=a[step[2]]
-    if C==False:
-        step[1]=a[step[1]]
+    print(a[i:i+4])
     if op == 99:
         #exit
         run=0
@@ -34,24 +19,18 @@ while i<len(a) and run == 1:
     else:
         if op == 4:
             #output
-            print(step[1])
+            print(a[i+1])
             i+=2
         if op == 3:
             #input
-            if A==False:
-                print(i,op)
-                a[step[1]]=int(input('> '))
-
+            a[a[i+1]]=int(input('> '))
             i+=2
         if op == 2:
             #multiply
-            if A==False:
-                a[step[3]]=step[1]*step[2]
+            a[a[i+3]]=a[i+1]*a[i+2]
             i+=4
         if op == 1:
             #add
-            if A==False:
-                a[step[3]]=step[1]+step[2]
+            a[a[i+3]]=a[i+1]+a[i+2]
             i+=4
-    print(i,op)
-    sleep(10)
+    input()
